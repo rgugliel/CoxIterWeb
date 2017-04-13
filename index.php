aea88b0569e489ff10073be267ff97d70b74a7e9
Rafael Guglielmetti <rafael@rgug.ch>
2017-04-13 11:00:05 +0200

    Footer

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ s0 s2 4</div>
 		</div>
 		<div id="content_footer"></div>
 		<div id="footer">
-			<p>Copyright &copy; shadowplay_2 | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.html5webtemplates.co.uk">design from HTML5webtemplates.co.uk</a></p>
+			<p>Design: <a href="http://www.html5webtemplates.co.uk">shadowplay_2</a> | CoxIter: Rafael Guglielmetti </p>
 		</div>
 	</div>
 	
